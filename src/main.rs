52f9ce122529660e5ec30afe1755125976c14ce8
rgutmann <ralph.gutmann@gmx.de>
2024-02-19 12:04:57 +0100

    added number formatting

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,42 @@ mod math_fun;
 use math_fun::*;
 
 use chrono::Local;
+use num_format::{Locale, ToFormattedString};
 use std::time::Instant;
 
-fn main() {
-    println!("\n{:?}\nPrime numbers 1..10000 calculation start...", Local::now());
-    let start_prime = Instant::now();
-    let primes_result = prime_numbers(1,10000);
-    let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
-    println!("Prime numbers found: {:?}", primes_result);
-    println!("<- Calculation took {}ms\n", duration_prime_millis);
+macro_rules! dbg {
+    ($x:expr) => {
+        println!("{} = {:?}",stringify!($x),$x);
+    }
+}
 
-    println!("\n{:?}\nPrime numbers with primes 1..10000 calculation start...", Local::now());
+fn format_number(number: u32) -> String {
+    number.to_formatted_string(&Locale::en)
+}
+/*
+impl U32 for u32 {
+    fn display(number: u32_d) -> String {
+        number.to_formatted_string(&Locale::en)
+    }
+}
+*/
+fn main() {
+    let debug = false;
+
+    let end = 10000u32;
+    println!("\n{:?}\nPrime numbers 1..{} calculation start...", Local::now(), format_number(end));
     let start_prime = Instant::now();
-    let primes_result = prime_numbers_with_primes(10000);
+    let primes_result = prime_numbers(end);
     let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
-    println!("Prime numbers found: {:?}", primes_result);
-    println!("<- Calculation took {}ms\n", duration_prime_millis);
+    if debug { println!("Prime numbers found: {:?}", primes_result); }
+    println!("<- Calculation of {} prime numbers took {}ms\n", format_number(primes_result.len() as u32), duration_prime_millis);
+
+    let end = 100000u32;
+    println!("\n{:?}\nPrime numbers with primes 1..{} calculation start...", Local::now(), format_number(end));
+    let start_prime = Instant::now();
+    let primes_result = prime_numbers_with_primes(end);
+    let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
+    if debug { println!("Prime numbers found: {:?}", primes_result); }
+    println!("<- Calculation of {} prime numbers took {}ms\n", format_number(primes_result.len() as u32), duration_prime_millis);
 
 }
--- src/math_fun.rs
+++ src/math_fun.rs
@@ -1,10 +1,12 @@
+#![allow(dead_code)]
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum MathError {
     EndBeforeStart,
 }
 
 /// Generating prime numbers with the simple optimization that only dividers until number / 2 need to be checked.
-pub fn prime_numbers(start: i32, end: i32) -> Result<Vec<i32>, MathError> {
+pub fn prime_numbers_between(start: u32, end: u32) -> Result<Vec<u32>, MathError> {
     if start > end {
         return Err(MathError::EndBeforeStart);
     }
@@ -28,8 +30,13 @@ pub fn prime_numbers(start: i32, end: i32) -> Result<Vec<i32>, MathError> {
     Ok(result)
 }
 
+/// Generating prime numbers with the simple optimization that only dividers until number / 2 need to be checked.
+pub fn prime_numbers(end: u32) -> Vec<u32> {
+    prime_numbers_between(1, end).expect("prime numbers returned")
+}
+
 /// Generating prime numbers with the optimization that only prime dividers need to be checked.
-pub fn prime_numbers_with_primes(end: i32) -> Vec<i32> {
+pub fn prime_numbers_with_primes(end: u32) -> Vec<u32> {
     let mut result = Vec::with_capacity(1024);
     for i in 1..(end + 1) {
         let mut found: bool = true;
@@ -53,22 +60,47 @@ pub fn prime_numbers_with_primes(end: i32) -> Vec<i32> {
     result
 }
 
+///  Generating prime numbers with the optimization that only prime dividers need to be checked.
+pub fn prime_numbers_with_primes_between(start: u32, end: u32) -> Result<Vec<u32>, MathError> {
+    if start > end {
+        return Err(MathError::EndBeforeStart);
+    }
+
+    let result = prime_numbers_with_primes(end);
+    let mut index = 0;
+    for (i, x) in result.iter().enumerate() {
+        if *x > start {
+            index = i;
+            break;
+        }
+    }
+
+    Ok(result[index..].to_vec())
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
+    fn test_primes_between() {
+        println!("Testing prime_numbers_between");
+        assert_eq!(prime_numbers_between(20, 1).err().expect("error returned"), MathError::EndBeforeStart);
+        assert_eq!(prime_numbers_between(1, 20), Ok(vec![1, 2, 3, 5, 7, 11, 13, 17, 19]));
+        assert_eq!(prime_numbers_between(4, 20), Ok(vec![5, 7, 11, 13, 17, 19]));
+    }
+
+    #[test]
     fn test_primes() {
         println!("Testing prime_numbers");
-        assert_eq!(prime_numbers(20, 1).err().expect("error returned"), MathError::EndBeforeStart);
-        assert_eq!(prime_numbers(1, 20), Ok(vec![1, 2, 3, 5, 7, 11, 13, 17, 19]));
-        assert_eq!(prime_numbers(4, 20), Ok(vec![5, 7, 11, 13, 17, 19]));
+        assert_eq!(prime_numbers(20), vec![1, 2, 3, 5, 7, 11, 13, 17, 19]);
     }
 
     #[test]
     fn test_primes_with_primes() {
         println!("Testing prime_numbers_with_primes");
-        assert_eq!(prime_numbers_with_primes(1000), prime_numbers(1, 1000).expect("primes returned"));
+        assert_eq!(prime_numbers_with_primes(1000), prime_numbers_between(1, 1000).expect("primes returned"));
+        assert_eq!(prime_numbers_with_primes_between(100, 1000).expect("primes returned"), prime_numbers_between(100, 1000).expect("primes returned"));
     }
 }
