cb008cc7b418f21dc13e1f5bc9ee24a802963c1c
rgutmann <ralph.gutmann@gmx.de>
2024-02-19 12:04:57 +0100

    prime numbers fun

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate chrono;
+
+mod math_fun;
+use math_fun::*;
+
+use chrono::Local;
+use std::time::Instant;
+
+fn main() {
+    println!("\n{:?}\nPrime numbers 1..10000 calculation start...", Local::now());
+    let start_prime = Instant::now();
+    let primes_result = prime_numbers(1,10000);
+    let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
+    println!("Prime numbers found: {:?}", primes_result);
+    println!("<- Calculation took {}ms\n", duration_prime_millis);
+
+}
---
+++ src/math_fun.rs
@@ -0,0 +1,32 @@
+
+pub fn prime_numbers(start: i32, end: i32) -> Vec<i32> {
+    let mut result = Vec::with_capacity(1000);
+    for i in start..end {
+        let mut found: bool = true;
+
+        for j in 2..(i / 2 + 1) {
+            if (i % j) == 0 {
+                found = false;
+                break;
+            }
+        }
+
+        if found {
+            result.push(i);
+        };
+    }
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_primes() {
+        println!("Testing prime_numbers");
+        assert_eq!(prime_numbers(1, 20), [1, 2, 3, 5, 7, 11, 13, 17, 19]);
+    }
+}
