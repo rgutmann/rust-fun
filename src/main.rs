0753a1a2efc6cfa9212056458da3480ca4c30792
rgutmann <ralph.gutmann@gmx.de>
2024-02-19 12:04:57 +0100

    math errors + prime divider optimization

--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,11 @@ fn main() {
     println!("Prime numbers found: {:?}", primes_result);
     println!("<- Calculation took {}ms\n", duration_prime_millis);
 
+    println!("\n{:?}\nPrime numbers with primes 1..10000 calculation start...", Local::now());
+    let start_prime = Instant::now();
+    let primes_result = prime_numbers_with_primes(10000);
+    let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
+    println!("Prime numbers found: {:?}", primes_result);
+    println!("<- Calculation took {}ms\n", duration_prime_millis);
+
 }
--- src/math_fun.rs
+++ src/math_fun.rs
@@ -1,7 +1,16 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MathError {
+    EndBeforeStart,
+}
 
-pub fn prime_numbers(start: i32, end: i32) -> Vec<i32> {
-    let mut result = Vec::with_capacity(1000);
-    for i in start..(end+1) {
+/// Generating prime numbers with the simple optimization that only dividers until number / 2 need to be checked.
+pub fn prime_numbers(start: i32, end: i32) -> Result<Vec<i32>, MathError> {
+    if start > end {
+        return Err(MathError::EndBeforeStart);
+    }
+
+    let mut result = Vec::with_capacity(1024);
+    for i in start..(end + 1) {
         let mut found: bool = true;
 
         for j in 2..(i / 2 + 1) {
@@ -16,7 +25,32 @@ pub fn prime_numbers(start: i32, end: i32) -> Vec<i32> {
         };
     }
 
-    return result;
+    Ok(result)
+}
+
+/// Generating prime numbers with the optimization that only prime dividers need to be checked.
+pub fn prime_numbers_with_primes(end: i32) -> Vec<i32> {
+    let mut result = Vec::with_capacity(1024);
+    for i in 1..(end + 1) {
+        let mut found: bool = true;
+
+        for j in &result {
+            if *j == 1 {
+                continue;
+            } else if *j < (i / 2 + 1) {
+                if (i % *j) == 0 {
+                    found = false;
+                    break;
+                }
+            } else { break; }
+        }
+
+        if found {
+            result.push(i);
+        };
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -27,6 +61,14 @@ mod tests {
     #[test]
     fn test_primes() {
         println!("Testing prime_numbers");
-        assert_eq!(prime_numbers(1, 20), vec![1, 2, 3, 5, 7, 11, 13, 17, 19]);
+        assert_eq!(prime_numbers(20, 1).err().expect("error returned"), MathError::EndBeforeStart);
+        assert_eq!(prime_numbers(1, 20), Ok(vec![1, 2, 3, 5, 7, 11, 13, 17, 19]));
+        assert_eq!(prime_numbers(4, 20), Ok(vec![5, 7, 11, 13, 17, 19]));
+    }
+
+    #[test]
+    fn test_primes_with_primes() {
+        println!("Testing prime_numbers_with_primes");
+        assert_eq!(prime_numbers_with_primes(1000), prime_numbers(1, 1000).expect("primes returned"));
     }
 }
