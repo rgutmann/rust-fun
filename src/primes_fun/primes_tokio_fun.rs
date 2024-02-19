f24dba60d441a8785c43393106ea2f9ec3f3b9f5
rgutmann <ralph.gutmann@gmx.de>
2024-02-19 12:04:57 +0100

    switch from task::spawn_blocking to task::spawn

--- src/primes_fun/primes_tokio_fun.rs
+++ src/primes_fun/primes_tokio_fun.rs
@@ -1,6 +1,6 @@
 use std::thread;
 use std::option::Option::{ Some, None };
-use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::sync::{Arc, Mutex};
 use std::sync::mpsc;
 use std::sync::mpsc::{Sender, Receiver};
 use std::collections::HashMap;
@@ -26,51 +26,41 @@ impl Hash for BlockResult {
     }
 }
 
-#[derive(Clone,Default)]
-struct PrimesBlockStorage {
-    map:Arc<RwLock<HashMap<u32, Option<Vec<u32>>>>>
-}
-impl PrimesBlockStorage {
-    pub fn read_lock(&self) -> RwLockReadGuard<'_, HashMap<u32, Option<Vec<u32>>>> {
-        self.map.read().unwrap()
-    }
-    pub fn write_lock(&self) -> RwLockWriteGuard<'_, HashMap<u32, Option<Vec<u32>>>> {
-        self.map.write().unwrap()
-    }
-}
-
 /// Calculate prime numbers in blocks concurrently.
 ///  Shows usage of:
-///  - using tokio io task async function impl
-///  - using tokio blocking tasks for cpu-bound work
-///  - creating a new thread
+///  - creating a new threads
+///  - using tokio task async function impl
 ///  - limiting task count by semaphore
-///  - mpsc channel communication
+///  - multi thread/task mpsc channel communication
+///  - using custom impls for HashMap objects
 ///  - concurrent access to a central HashMap storage
-///  - default init for structs
 pub async fn prime_numbers_with_tokio(start: u32, end: u32, block_size: usize) -> Vec<u32> {
 
     // create reference counter for limiting worker task count to number of cpu cores
-    let num_cpus = 1; // TODO num_cpus::get();
+    let num_cpus = num_cpus::get();
     println!("Found {num_cpus} cpus - using this as max concurrent task count");
     let sem = Arc::new(Semaphore::new(num_cpus));
 
-    // create send/receiver for channel communication between worker tasks and storage thread
-    let (tx, rx) : (Sender<BlockResult>, Receiver<BlockResult>)= mpsc::channel();
+    // create central storage
+    let db :Arc<Mutex<HashMap<u32, Option<Vec<u32>>>>> = Arc::new(Mutex::new(HashMap::new()));
 
-    let block_storage = PrimesBlockStorage {..Default::default()};
+    // create send/receiver for channel communication between worker tasks and storage thread
+    let (tx, rx) :(Sender<BlockResult>, Receiver<BlockResult>) = mpsc::channel();
+
+    //let block_storage = PrimesBlockStorage {..Default::default()};
     // create storage thread
-    let storage = block_storage.clone();
+    let db_handle = db.clone();
     thread::spawn(move || {
         loop {
             match rx.recv() {
                 Ok(result) => {
                     match result.result {
-                        Some(ref block) => println!("< received block {:?} with {} primes", result.block_start, block.len()),
-                        None => println!("- received block {:?} for which calculation started", result.block_start)
+                        Some(ref block) => println!("<DB received block {:?} with {} primes", result.block_start, block.len()),
+                        None => println!("-DB received block {:?} for which calculation started", result.block_start)
                     }
                     // insert/update to storage in any case
-                    storage.clone().write_lock().insert(result.block_start, result.result);
+                    let mut db = db_handle.lock().unwrap();
+                    db.insert(result.block_start, result.result);
                 },
                 Err(error) => {
                     println!("received error {:?}", error);
@@ -85,8 +75,8 @@ pub async fn prime_numbers_with_tokio(start: u32, end: u32, block_size: usize) -
         let tx_clone = mpsc::Sender::clone(&tx);
         let permit = sem.clone().acquire_owned().await.unwrap();
         println!("Acquired one permit, currently {} are still available", sem.as_ref().available_permits());
-        let storage = block_storage.clone();
-        task::spawn_blocking(move || {
+        let db_handle = db.clone();
+        task::spawn(async move {
             // always calculate the whole block
             let block_end = block_start + (block_size as u32) - 1;
             println!("Calculating {}..{} primes block", block_start, block_end);
@@ -113,40 +103,49 @@ pub async fn prime_numbers_with_tokio(start: u32, end: u32, block_size: usize) -
                     // always start with the first precalc block and then work upwards
                     // check if I am this block => the first and last block...
                     let mut needed_precalc_block_start = 1;
+                    let mut wait = false;
 
                     // I'm not the first and will use previous precalc_blocks
                     loop { // over all precalc_blocks
 
                         // get new block, wait for it if necessary
+                        if wait {
+                            println!("  - block {block_start} waiting for precalc block {needed_precalc_block_start}");
+                            tokio::time::sleep(Duration::from_millis(10)).await;
+                            wait = false;
+                        }
                         loop {
-                            match storage.read_lock().get(&needed_precalc_block_start) {
+                            let precalc_block_clone :Vec<u32>;
+                            match db_handle.lock().unwrap().get(&needed_precalc_block_start) {
                                 None | Some(None) => {
                                     // precalc_block not found or in calculation
-                                    println!("  - waiting for precalc block {needed_precalc_block_start}");
-                                    thread::sleep(Duration::from_millis(10));
+                                    wait = true;
                                     continue; // continue waiting for this precalc_block
                                 },
                                 Some(Some(ref precalc_block_result)) => {
-                                    //println!("  - now working with precalc block {needed_precalc_block_start}");
-
-                                    // use precalc block content and always check if we're finished with this prime calc
-                                    // iterate through precalc_block until end of it is reached
-                                    for precalc_prime in (*precalc_block_result).clone() {
-                                        // test prime_to_check against precalc_prime
-                                        if precalc_prime != 1 && prime_to_check % precalc_prime == 0 {
-                                            // prime_to_check is not a prime
-                                            prime_found = false;
-                                            break;
-                                        }
-                                        // always check if prime calc is finished yet
-                                        if precalc_prime > max_divider {
-                                            break;
-                                        }
-                                    }
-                                    // end of precalc_block reading & usage - we're done here
-                                    break; // into next block reading
+                                    precalc_block_clone = (*precalc_block_result).clone();
+                                    //println!("  - block {block_start} now loaded precalc block {needed_precalc_block_start}");
                                 }
                             }
+
+                            // use precalc block content and always check if we're finished with this prime calc
+                            // iterate through precalc_block until end of it is reached
+                            // TODO move this to task::spawn_blocking() ?!?!
+                            for precalc_prime in precalc_block_clone {
+                                // test prime_to_check against precalc_prime
+                                if precalc_prime != 1 && prime_to_check % precalc_prime == 0 {
+                                    // prime_to_check is not a prime
+                                    prime_found = false;
+                                    break;
+                                }
+                                // always check if prime calc is finished yet
+                                if precalc_prime > max_divider {
+                                    break;
+                                }
+                            }
+                            // end of precalc_block reading & usage - we're done here
+                            break; // into next block reading
+
                         } // end of loop over block reading
 
                         if !prime_found || (needed_precalc_block_start + (block_size as u32)) > max_divider {
@@ -191,9 +190,9 @@ pub async fn prime_numbers_with_tokio(start: u32, end: u32, block_size: usize) -
     let first_block_start = start - ((start-1) % (block_size as u32));
     let last_block_start = end - ((end-1) % (block_size as u32));
     println!("Collecting blocks from {first_block_start} to {last_block_start} with step {block_size}");
-    let storage = block_storage.clone();
+    let db_handle = db.clone();
     for block_start in (first_block_start..=last_block_start).step_by(block_size) {
-        match storage.read_lock().get(&block_start) {
+        match db_handle.lock().unwrap().get(&block_start) {
             None | Some(None) => {
                 // precalc_block not found or in calculation
                 panic!("block {} not found after calc is finished?!? this should never happen!", block_start)
@@ -211,16 +210,23 @@ pub async fn prime_numbers_with_tokio(start: u32, end: u32, block_size: usize) -
 
 #[cfg(test)]
 mod tests {
-    use crate::prime_numbers_with_primes_between;
+    use std::time::Instant;
+    use crate::{format_number, prime_numbers_with_primes_between};
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[tokio::test]
     async fn test_primes_redis() {
         println!("Testing prime_numbers with tokio");
-        let result = prime_numbers_with_tokio(1, 10000, 1000).await;
-        assert_eq!(result, prime_numbers_with_primes(10000));
-        let result = prime_numbers_with_tokio(123, 9876, 1000).await;
-        assert_eq!(result, prime_numbers_with_primes_between(123, 9876).unwrap());
+        let result = prime_numbers_with_tokio(1, 100000, 20000).await;
+        assert_eq!(result, prime_numbers_with_primes(100000));
+
+        let start_tokio = Instant::now();
+        let result = prime_numbers_with_tokio(1234, 198765, 20000).await;
+        let duration_tokio_millis = Instant::now().duration_since(start_tokio).as_millis();
+        let start_prime = Instant::now();
+        assert_eq!(result, prime_numbers_with_primes_between(1234, 198765).unwrap());
+        let duration_prime_millis = Instant::now().duration_since(start_prime).as_millis();
+        println!("<- Calculation of {} prime numbers took {}ms with tokio and {}ms without\n", format_number(result.len() as u32), duration_tokio_millis, duration_prime_millis);
     }
 }
